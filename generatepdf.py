4ad4f2031f0d5e344372bdbda67e90628fed0d4b
sarrvesh <sarrvesh.ss@gmail.com>
2019-10-04 15:44:45 +0200

    Write PDF output in a table format

--- generatepdf.py
+++ generatepdf.py
@@ -1,4 +1,6 @@
-from fpdf import FPDF
+from fpdf import FPDF, HTMLMixin
+
+class MyFPDF(FPDF, HTMLMixin): pass
 
 def generatepdf(pdffile, obsT, nCore, nRemote, nInt, nChan, nSb, integT, 
                 antSet, pipeType, tAvg, fAvg, isDysco, imNoiseVal, rawSize, 
@@ -6,41 +8,59 @@ def generatepdf(pdffile, obsT, nCore, nRemote, nInt, nChan, nSb, integT,
    """Function to generate a pdf file summarizing the content of the calculator.
        Return nothing."""   
    # Create an A4 sheet
-   pdf = FPDF('P', 'mm', 'A4')
+   pdf = MyFPDF('P', 'mm', 'A4')
    pdf.add_page()
    pdf.set_font('Arial', '', 16)
    
-   # Generate string to be written to the file
-   string  = 'Observation time (in seconds): \t {}\n'.format(obsT)
-   string += 'No. of stations: \t\t\t ({}, {}, {})\n'.format(nCore, nRemote, nInt)
-   string += 'No. of subbands:\t {}\n'.format(nSb)
-   string += 'No. of channels per subband:\t {}\n'.format(nChan)
-   string += 'Correlator integration time:\t {}\n'.format(integT)
+   # Generate an html string to be written to the file
+   string  = '<table border="0" align="left" width="80%">'
+   string += '<thead><tr><th width="70%" align="left">Parameter</th>'
+   string += '<th width="30%" align="left">Value</th></tr></thead>'
+   string += '<tbody>'
+   string += '<tr><td>Observation time (in seconds)</td>'
+   string += '    <td>{}</td></tr>'.format(obsT)
+   string += '<tr><td>No. of stations</td>'
+   string += '    <td>({}, {}, {})</td></tr>'.format(nCore, nRemote, nInt)
+   string += '<tr><td>No. of subbands</td>'
+   string += '    <td>{}</td></tr>'.format(nSb)
+   string += '<tr><td>No. of channels per subband</td>'
+   string += '    <td>{}</td></tr>'.format(nChan)
+   string += '<tr><td>Integration time (in seconds)</td>'
+   string += '    <td>{}</td></tr>'.format(integT)
    if antSet == 'enable':
       mode = 'HBA'
    else:
       mode = 'LBA'
-   string += 'Antenna set:\t {}\n'.format(mode)
-   string += '\n'
+   string += '<tr><td>Antenna set</td>'
+   string += '    <td>{}</td></tr>'.format(mode)
+   string += '<tr></tr>'
+   string += '<tr><td>Pipeline type</td>'
    if pipeType == 'none':
-      string += 'Pipeline type:\t None\n'
+      string += '    <td>{}</td></tr>'.format('None')
    else:
-      string += 'Pipeline type:\t Preprocessing\n'
-      string += 'Averaging factor (time, freq):\t {}, {}\n'.format(tAvg, fAvg)
+      string += '    <td>{}</td></tr>'.format('Preprocessing')
+      string += '<tr><td>Averaging factor (time, freq)</td>'
+      string += '    <td>{}, {}</td></tr>'.format(tAvg, fAvg)
+      string += '<tr><td>Dysco compression</td>'
       if isDysco == 'enable':
-         mode = 'enabled'
+         string += '    <td>{}</td></tr>'.format('enabled')
       else:
-         mode = 'disabled'
-      string += 'Dysco compression:\t {}\n'.format(mode)
-   string += '\n'
-   string += 'Theoretical image sensitivity (uJy/beam): \t {}\n'.format(imNoiseVal)
-   string += 'Raw data size (in GB):\t {}\n'.format(rawSize)
+         string += '    <td>{}</td></tr>'.format('disabled')
+   string += '<tr></tr>'
+   string += '<tr><td>Theoretical image sensitivity (uJy/beam)</td>'
+   string += '    <td>{}</td></tr>'.format(imNoiseVal)
+   string += '<tr><td>Raw data size (in GB)</td>'
+   string += '    <td>{}</td></tr>'.format(rawSize)
    if pipeType != 'none':
-      string += 'Processed data size (in GB):\t {}\n'.format(procSize)
-      string += 'Pipeline processing time (in hours):\t {}\n'.format(pipeTime)
+      string += '<tr><td>Processed data size (in GB)</td>'
+      string += '    <td>{}</td></tr>'.format(procSize)
+      string += '<tr><td>Pipeline processing time (in hours)</td>'
+      string += '    <td>{}</td></tr>'.format(pipeTime)
+   string += '</tbody>'
+   string += '</table>'
    
    # Write text to the pdf file
-   pdf.write(7, txt=string)
+   pdf.write_html(string)
    
    # Write the pdf to disk
    pdf.output(pdffile)
