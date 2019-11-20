44b11147ea51187ffdaadf8f0de7e37795219837
sarrvesh <sarrvesh@stationmonitor.control.lofar>
2019-11-20 14:33:58 +0000

    fix app setting for proper web hosting

--- calculator.py
+++ calculator.py
@@ -19,7 +19,9 @@ import generatepdf as g
 # Initialize the dash app
 server = flask.Flask(__name__)
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUMEN], \
-                server=server)
+                server=server, url_base_pathname='/luci/')
+app.css.config.serve_locally = True
+app.scripts.config.serve_locally = True
 
 #######################################
 # Setup the layout of the web interface
